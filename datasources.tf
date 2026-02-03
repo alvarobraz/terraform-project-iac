68dd6db6477336cbe621c09bf4630bcbcb6f09ec
Álvaro Braz <alvarobraz83@gmail.com>
2026-02-03 14:42:06 -0300

    feat: variables

--- datasources.tf
+++ datasources.tf
@@ -1,5 +1,5 @@
 data "aws_s3_bucket" "bucket" {
-  bucket = "aab-bucket-iac-03022026-${terraform.workspace}"
+  bucket = "${var.org_name}-bucket-iac-03022026-${terraform.workspace}"
   
   depends_on = [aws_s3_bucket.bucket]
 }
--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "bucket" {
   # Adicione o workspace aqui também para coincidir com o data source
-  bucket = "aab-bucket-iac-03022026-${terraform.workspace}"
+  bucket = "${var.org_name}-bucket-iac-03022026-${terraform.workspace}"
 
   tags = {
     Name = "Primeiro bucket"
