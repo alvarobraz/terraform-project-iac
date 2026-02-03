1746d87ce48831985663bb25ae98b6d37771a5af
Álvaro Braz <alvarobraz83@gmail.com>
2026-02-03 14:35:22 -0300

    feat: outputs

--- datasources.tf
+++ datasources.tf
@@ -1,3 +1,5 @@
 data "aws_s3_bucket" "bucket" {
-    bucket = "aab-bucket-iac-03022026-${terraform.workspace}"
+  bucket = "aab-bucket-iac-03022026-${terraform.workspace}"
+  
+  depends_on = [aws_s3_bucket.bucket]
 }
--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 resource "aws_s3_bucket" "bucket" {
-  bucket = "aab-bucket-iac-03022026-staging"
+  # Adicione o workspace aqui também para coincidir com o data source
+  bucket = "aab-bucket-iac-03022026-${terraform.workspace}"
 
   tags = {
-    Name    = "Primeiro bucket"
-    Iac     = "true"
+    Name = "Primeiro bucket"
+    Iac  = "true"
   }
 }
